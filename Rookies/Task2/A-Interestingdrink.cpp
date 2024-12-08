dfa2a272c5fb48d68ade10d3003030ce541e763b
NorhanElyann <norhanelyan@gmail.com>
2024-12-08 13:23:20 +0200

    solve problem A

--- Rookies/Task2/A-Interestingdrink.cpp
+++ Rookies/Task2/A-Interestingdrink.cpp
@@ -1,1 +1,23 @@
-about task2
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, q;
+    cin >> n;
+    int a1[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a1[i];
+    }
+    sort(a1, a1 + n);
+    cin >> q;
+    while (q--) {
+        int qn;
+        cin >> qn;
+        int count = upper_bound(a1, a1 + n, qn) - a1;
+        cout<<count<<endl;
+    }
+    return 0;
+}
+
